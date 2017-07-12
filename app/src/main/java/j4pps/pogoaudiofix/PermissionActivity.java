969ab21afc47866e30dca71b3a50b0152dc49c62
Tobias Weihrauch <jagie4ever@hotmail.com>
2017-07-12 22:08:51 +0200

    added ability to generate debugging logs

--- app/src/main/java/j4pps/pogoaudiofix/PermissionActivity.java
+++ app/src/main/java/j4pps/pogoaudiofix/PermissionActivity.java
@@ -1,10 +1,13 @@
 package j4pps.pogoaudiofix;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
@@ -24,28 +27,35 @@ public class PermissionActivity extends AppCompatActivity {
 
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
 
-        SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar);
-        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                int time = 1000 + 500 * progress;
-                String s = String.valueOf((float) time / 1000f) + "s";
-                ((TextView) findViewById(R.id.textView_secs)).setText(s);
+        try {
+            SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar);
+            seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    int time = 1000 + 500 * progress;
+                    String s = String.valueOf((float) time / 1000f) + "s";
+                    ((TextView) findViewById(R.id.textView_secs)).setText(s);
 
-            }
+                }
 
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {}
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+                }
 
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                int time = 1000 + 500 * seekBar.getProgress();
-                SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).edit();
-                editor.putInt(VolumeCheckService.PREF_CHECK_PAUSE, time);
-                editor.apply();
-            }
-        });
-        seekBar.setProgress((preferences.getInt(VolumeCheckService.PREF_CHECK_PAUSE, 1000) - 1000)/500);
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+                    int time = 1000 + 500 * seekBar.getProgress();
+                    SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).edit();
+                    editor.putInt(VolumeCheckService.PREF_CHECK_PAUSE, time);
+                    editor.apply();
+                }
+            });
+            seekBar.setProgress((preferences.getInt(VolumeCheckService.PREF_CHECK_PAUSE, 1000) - 1000) / 500);
+        } catch (Exception e) {
+            Log.e(TAG, "Exception setting up SeekBar:");
+            e.printStackTrace();
+            askToGenerateDebugLogs();
+        }
     }
 
     @Override
@@ -54,31 +64,64 @@ public class PermissionActivity extends AppCompatActivity {
         updateButtons();
     }
 
+    private void askToGenerateDebugLogs() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Debug logs")
+                .setMessage("There seems to be some kind of error. Do you want to generate some debugging logs?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        AndroidUtils.sendDebugEmail(getApplicationContext(), null);
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                }).create().show();
+    }
+
     private void updateButtons() {
         Button button;
+        boolean notification = false;
+        boolean usage = false;
 
-        boolean notification = AndroidUtils.hasNotificationAccess(this);
-        button = (Button) findViewById(R.id.button_notifcation);
-        if(notification) {
-            button.setText("Granted");
-            button.setTextColor(AndroidUtils.getColorFromResources(this, R.color.colorGranted));
-            button.setEnabled(false);
-        } else {
-            button.setText("Declined");
-            button.setTextColor(AndroidUtils.getColorFromResources(this, R.color.colorDenied));
-            button.setEnabled(true);
+        try {
+            notification = AndroidUtils.hasNotificationAccess(this);
+            button = (Button) findViewById(R.id.button_notifcation);
+            if(notification) {
+                button.setText("Granted");
+                button.setTextColor(AndroidUtils.getColorFromResources(this, R.color.colorGranted));
+                button.setEnabled(false);
+            } else {
+                button.setText("Declined");
+                button.setTextColor(AndroidUtils.getColorFromResources(this, R.color.colorDenied));
+                button.setEnabled(true);
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Exception updating notification button:");
+            e.printStackTrace();
+            askToGenerateDebugLogs();
         }
 
-        boolean usage = AndroidUtils.hasUsageStatsPermission(this);
-        button = (Button) findViewById(R.id.button_usage);
-        if(usage) {
-            button.setText("Granted");
-            button.setTextColor(AndroidUtils.getColorFromResources(this, R.color.colorGranted));
-            button.setEnabled(false);
-        } else {
-            button.setText("Declined");
-            button.setTextColor(AndroidUtils.getColorFromResources(this, R.color.colorDenied));
-            button.setEnabled(true);
+        try {
+            usage = AndroidUtils.hasUsageStatsPermission(this);
+            button = (Button) findViewById(R.id.button_usage);
+            if(usage) {
+                button.setText("Granted");
+                button.setTextColor(AndroidUtils.getColorFromResources(this, R.color.colorGranted));
+                button.setEnabled(false);
+            } else {
+                button.setText("Declined");
+                button.setTextColor(AndroidUtils.getColorFromResources(this, R.color.colorDenied));
+                button.setEnabled(true);
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Exception updating usage button:");
+            e.printStackTrace();
+            askToGenerateDebugLogs();
         }
 
         button = (Button) findViewById(R.id.button_start);
--- app/src/main/java/util/AndroidUtils.java
+++ app/src/main/java/util/AndroidUtils.java
@@ -1,15 +1,31 @@
 package util;
 
 import android.Manifest;
+import android.app.ActivityManager;
 import android.app.AppOpsManager;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Point;
+import android.net.Uri;
 import android.os.Build;
 import android.provider.Settings;
+import android.support.v4.content.FileProvider;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FilenameFilter;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Locale;
+
+import j4pps.pogoaudiofix.BuildConfig;
 
 public class AndroidUtils {
 
@@ -85,6 +101,64 @@ public class AndroidUtils {
         } catch (Exception e) {
             e.printStackTrace();
             return false;
+        }
+    }
+
+    public static void sendDebugEmail(Context context, String address) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        String[] emailAddresses = new String[1];
+        emailAddresses[0] = address;
+        intent.putExtra(Intent.EXTRA_EMAIL, emailAddresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, "PoGO Audio Fix debug");
+
+        ArrayList<Uri> uris = new ArrayList<>();
+
+        //logs
+            //add some info to email
+            StringBuilder text = new StringBuilder();
+            text.append("\n\n\n\n\n********** Device info **********").append(System.lineSeparator());;
+            text.append("App version: ").append(BuildConfig.VERSION_NAME).append(" / ").append(BuildConfig.VERSION_CODE).append(System.lineSeparator());
+            text.append("Device:\t\t").append(Build.DEVICE).append(System.lineSeparator());
+            text.append("Manufacturer:\t").append(Build.MANUFACTURER).append(System.lineSeparator());
+            text.append("Model:\t\t").append(Build.MODEL).append(System.lineSeparator());
+
+            //memory info
+            ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+            ActivityManager.MemoryInfo memoryInfo = new ActivityManager.MemoryInfo();
+            activityManager.getMemoryInfo(memoryInfo);
+            text.append(String.format(Locale.ENGLISH, "Total RAM:\t\t%.2f GB", (float) memoryInfo.totalMem / 1000000000)).append(System.lineSeparator());
+            text.append(String.format(Locale.ENGLISH, "Available RAM:\t\t%.2f MB", (float) memoryInfo.availMem / 1000000)).append(System.lineSeparator());
+            text.append(String.format(Locale.ENGLISH, "Threshold:\t\t%.2f MB", (float) memoryInfo.threshold  / 1000000)).append(System.lineSeparator());
+            text.append("***********************************")
+            .append(System.lineSeparator())
+            .append(System.lineSeparator());
+
+            intent.putExtra(Intent.EXTRA_TEXT, text.toString());
+
+            BufferedReader bufferedReader = null;
+
+            try {
+                Process process = Runtime.getRuntime().exec("logcat -d");
+                bufferedReader = new BufferedReader(
+                        new InputStreamReader(process.getInputStream()));
+
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    text.append(line);
+                    text.append("\n");
+                }
+            } catch (Exception e) {
+                Log.e(TAG, "Error saving logs:");
+                e.printStackTrace();
+                Toast.makeText(context, "Could not generate debugging logs", Toast.LENGTH_SHORT).show();
+            }
+
+        try {
+            context.startActivity(intent);
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(context, "\"It seems that you do not have an email app installed.\"", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
         }
     }
 }
